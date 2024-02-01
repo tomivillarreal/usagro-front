7152789ba193385affaf7779d031bf4f20cd5b4b
Tomás Villarreal <134805538+tomivillarreal@users.noreply.github.com>
2024-02-01 20:52:30 -0300

    feat: incorporacion de contacto e inventario

---
+++ app/contacto/page.tsx
@@ -0,0 +1,8 @@
+export default function Contacto(){
+
+    return(
+        <div>
+            contacto
+        </div>
+    )
+}
---
+++ app/inventario/page.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function Inventario(){
+
+    return(
+        <div>
+            inventario
+        </div>
+    )
+}
