248cad6b5cbb77f1ee754983e92caf65dfba9876
Tomás Villarreal <134805538+tomivillarreal@users.noreply.github.com>
2024-02-01 23:22:44 -0300

    sharp

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    output: 'export',
-   
+    // output: 'export',
+    // target: 'serverless',
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
    
