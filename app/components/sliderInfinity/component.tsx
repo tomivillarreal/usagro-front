0ce2bbae5c67f9dc01f738d7c9b3b91535896926
Tomás Villarreal <134805538+tomivillarreal@users.noreply.github.com>
2024-02-03 12:08:37 -0300

    feat: :lipstick: incorporacion de mas imagenes y scroll responsive

--- app/components/sliderInfinity/component.tsx
+++ app/components/sliderInfinity/component.tsx
@@ -8,6 +8,19 @@ import marca5 from "/public/images/marcas/marca5.png";
 import marca6 from "/public/images/marcas/marca6.png";
 import marca7 from "/public/images/marcas/marca7.png";
 import marca8 from "/public/images/marcas/marca8.png";
+import marca9 from "/public/images/marcas/marca9.png";
+import marca10 from "/public/images/marcas/marca10.png";
+import marca11 from "/public/images/marcas/marca11.png";
+import marca12 from "/public/images/marcas/marca12.png";
+import marca13 from "/public/images/marcas/marca13.png";
+import marca14 from "/public/images/marcas/marca14.png";
+import marca15 from "/public/images/marcas/marca15.png";
+import marca16 from "/public/images/marcas/marca16.png";
+import marca17 from "/public/images/marcas/marca17.png";
+import marca18 from "/public/images/marcas/marca18.jpeg";
+import marca19 from "/public/images/marcas/marca19.jpg";
+import marca20 from "/public/images/marcas/marca20.png";
+import marca21 from "/public/images/marcas/marca21.png";
 export default function SliderInfinity() {
   return (
     <div>
@@ -95,7 +108,7 @@ export default function SliderInfinity() {
           </div>
           <div className="slide">
             <Image
-              src={marca1}
+              src={marca9}
               layout="responsive"
               width={1000}
               height={1000}
@@ -105,7 +118,7 @@ export default function SliderInfinity() {
           </div>
           <div className="slide">
             <Image
-              src={marca2}
+              src={marca10}
               layout="responsive"
               width={1000}
               height={1000}
@@ -115,7 +128,7 @@ export default function SliderInfinity() {
           </div>
           <div className="slide">
             <Image
-              src={marca3}
+              src={marca11}
               layout="responsive"
               width={1000}
               height={1000}
@@ -125,7 +138,7 @@ export default function SliderInfinity() {
           </div>
           <div className="slide">
             <Image
-              src={marca4}
+              src={marca12}
               layout="responsive"
               width={1000}
               height={1000}
@@ -135,7 +148,87 @@ export default function SliderInfinity() {
           </div>
           <div className="slide">
             <Image
-              src={marca5}
+              src={marca13}
+              layout="responsive"
+              width={1000}
+              height={1000}
+              alt="portada"
+              className="object-cover"
+            ></Image>
+          </div>
+          <div className="slide">
+            <Image
+              src={marca14}
+              layout="responsive"
+              width={1000}
+              height={1000}
+              alt="portada"
+              className="object-cover"
+            ></Image>
+          </div>
+          <div className="slide">
+            <Image
+              src={marca15}
+              layout="responsive"
+              width={1000}
+              height={1000}
+              alt="portada"
+              className="object-cover"
+            ></Image>
+          </div>
+          <div className="slide">
+            <Image
+              src={marca16}
+              layout="responsive"
+              width={1000}
+              height={1000}
+              alt="portada"
+              className="object-cover"
+            ></Image>
+          </div>
+          <div className="slide">
+            <Image
+              src={marca17}
+              layout="responsive"
+              width={1000}
+              height={1000}
+              alt="portada"
+              className="object-cover"
+            ></Image>
+          </div>
+          <div className="slide">
+            <Image
+              src={marca18}
+              layout="responsive"
+              width={1000}
+              height={1000}
+              alt="portada"
+              className="object-cover"
+            ></Image>
+          </div>
+          <div className="slide">
+            <Image
+              src={marca19}
+              layout="responsive"
+              width={1000}
+              height={1000}
+              alt="portada"
+              className="object-cover"
+            ></Image>
+          </div>
+          <div className="slide">
+            <Image
+              src={marca20}
+              layout="responsive"
+              width={1000}
+              height={1000}
+              alt="portada"
+              className="object-cover"
+            ></Image>
+          </div>
+          <div className="slide">
+            <Image
+              src={marca21}
               layout="responsive"
               width={1000}
               height={1000}
